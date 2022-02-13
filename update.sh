47af4c7c6e87d6e09aace201e14728b96c15d058
unknown <52136571+wolfgang-degroot@users.noreply.github.com>
2022-02-13 17:00:50 -0600

    Check if update is needed before pulling

--- update.sh
+++ update.sh
@@ -4,9 +4,18 @@
 # │ by Wolfgang de Groot │
 # └──────────────────────┘
 
-# 1. Update repository from GitHub
-echo "[] Updating from GitHub..."
-git pull
+# 1. Check if an update is needed
+LOCAL=$(git rev-parse HEAD)
+REPOS=$(curl -s "https://api.github.com/repos/Intro-to-SE-lab-Spring-22/Group-8/branches/main" | jq -r ".commit.sha")
+
+if [ "$LOCAL" == "$REPOS" ]; then
+    # 1a. Do nothing.
+    echo "[] Already up to date."
+else
+    # 1b. Update repository from GitHub
+    echo "[] Updating from GitHub..."
+    git pull
+fi
 
 # 2. Composer updates and deploys all dependencies/requirements
 echo "[] Deploying project..."
