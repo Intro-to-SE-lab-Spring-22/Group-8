8a705bdb1332a56ecab8ba4f418b1cd2f1938b65
adb1042 <adb1042@msstate.edu>
2022-03-04 15:03:49 -0600

    Demonstration purposes to show the group how to use branches and pull requests
    Demonstration purposes to show the group how to use branches and pull requests

---
+++ src/LogVer.php
@@ -0,0 +1,20 @@
+<?php
+namespace Group8\Spyke; # This namespace must be kept unchanged.
+
+class ClassTemplate # MUST be the same as the filename in PascalCase (sans '.php')
+{
+    public function helloWorld() # Name your methods in camelCase
+    {
+		# An example on how to make "Howdy!" happen in /public/index.php
+		# OOP Method:
+		#	$Class = new \Group8\Spyke\ClassTemplate;
+		#	$Class->helloWorld();
+        print("Howdy!");
+    }
+
+    public function checkDB(string Usrname, string pwd ){
+    	
+    }
+}
+
+
