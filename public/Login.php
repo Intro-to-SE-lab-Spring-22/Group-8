4f7f928e82968708ba89cb9906e63393e9365a9f
adb1042 <adb1042@msstate.edu>
2022-03-04 16:50:41 -0600

    Login and register.php relocation
    Moving login.php and register.php to their correct locations, that way they can be accessible by users

---
+++ public/Login.php
@@ -0,0 +1,5 @@
+<?php
+# Login page for Spyke, should take user input in fields "Username" and "Password" to verify that the user and password combination exists in the database, on fail: "This Username does not exist, or this password does not match this Username"  on success: print to the screen "welcome <username> and call make a call to the homepage with that user's credentials to fill out their own homepage.
+
+
+?>
