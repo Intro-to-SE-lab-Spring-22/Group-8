645bb45d4ac8d872083f5876f5d8d72e84bc3569
adb1042 <adb1042@msstate.edu>
2022-03-05 17:10:40 -0600

    UserPage.php Creation and resources addition
    UserPage.php
     -- HTML Written
     -- WIP image
     -- UserPage.css in assets/css

---
+++ public/UserPage.php
@@ -0,0 +1,22 @@
+<? php
+ 
+
+
+?>
+
+
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>User Page</title>
+</head>
+<body>
+	<!-- This page is a work in progress, and will be worked on in sprint 3. -->
+	
+	<img src="assets/img/Spyke_WIP.png">
+
+</body>
+</html>
