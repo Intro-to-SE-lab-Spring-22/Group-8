9f3ebf9bfd526c2bde08cef35fe5208d444876bf
wolfgang-degroot <52136571+wolfgang-degroot@users.noreply.github.com>
2022-03-06 19:11:35 -0600

    Replace bad whitespace with good whitespace

--- src/Debug.php
+++ src/Debug.php
@@ -6,12 +6,14 @@ class Debug
 	const DIR = __DIR__ . "/../log/"; // Log Directory
 	private int $id;
 	private string $user;
+	private bool $logUser;
 
-    public function __construct(string $user = "Anonymous", int $id = null) # Name your methods in camelCase
+    public function __construct(string $user = "Anonymous", int $id = null)
     {
 		// Set values (or default to '-')
 		$this->id = (is_int($id) ? $id : -1);
         $this->user = preg_replace("/\s+/", "_", $user);
+		$this->logUser = true;
 
 		// Create a folder if needed
 		if (!is_dir(self::DIR)) {
@@ -25,7 +27,7 @@ class Debug
 		$date =	date("Y-m-d");
 		$id =	$this->id  >= 0 ? $this->id : "-";
 		$time =	date("[H:i:s]");
-		$body =	(is_string($from) ? "[{$from}] " : null) . $message;
+		$body =	(is_string($from) ? "[{$from}] " : null) . trim(preg_replace("/\s+/", " ", $message));
 
 		// Status construction block
 		if (is_int($httpStatusInt)) {
