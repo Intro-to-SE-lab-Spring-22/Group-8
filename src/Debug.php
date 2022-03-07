1ff79c29066174856fe70725fea12e493c55f04c
wolfgang-degroot <52136571+wolfgang-degroot@users.noreply.github.com>
2022-03-06 19:04:09 -0600

    Create Debug.php

---
+++ src/Debug.php
@@ -0,0 +1,46 @@
+<?php
+namespace Group8\Spyke;
+
+class Debug
+{
+	const DIR = __DIR__ . "/../log/"; // Log Directory
+	private int $id;
+	private string $user;
+
+    public function __construct(string $user = "Anonymous", int $id = null) # Name your methods in camelCase
+    {
+		// Set values (or default to '-')
+		$this->id = (is_int($id) ? $id : -1);
+        $this->user = preg_replace("/\s+/", "_", $user);
+
+		// Create a folder if needed
+		if (!is_dir(self::DIR)) {
+			mkdir(self::DIR, 0777, true);
+		}
+    }
+
+    public function log(string $message, bool $error = false, string $from = null, int $httpStatusInt = null)
+	{
+		// Initialize variables
+		$date =	date("Y-m-d");
+		$id =	$this->id  >= 0 ? $this->id : "-";
+		$time =	date("[H:i:s]");
+		$body =	(is_string($from) ? "[{$from}] " : null) . $message;
+
+		// Status construction block
+		if (is_int($httpStatusInt)) {
+			$httpStatus = str_pad($httpStatusInt, 3, "0", STR_PAD_LEFT);
+		} else {
+			$httpStatus = "-";
+		}
+
+		// $log construction gets its own block
+		$log =	self::DIR . ($error ? "error" : "event") . "-{$date}.log";
+
+		// Construct an NCSA Common Log-formatted string
+		$format = "- {$id} {$this->user}\t{$time} \"{$body}\" {$httpStatus} -\n";
+
+		// Write to disk (UTF-8)
+		file_put_contents($log, utf8_encode($format),  FILE_APPEND | LOCK_EX);
+	}
+}
