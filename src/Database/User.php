da839e60cbf29bfdf8a56cdff26195bf3b4432ae
wolfgang-degroot <52136571+wolfgang-degroot@users.noreply.github.com>
2022-03-08 11:48:00 -0600

    getUserList() for an array
    [id] => [username]

--- src/Database/User.php
+++ src/Database/User.php
@@ -14,4 +14,17 @@ class User extends \Group8\Spyke\Database
 		$sql = "INSERT INTO users (username, id, pass) VALUES (:username, :id, :pass)";
 		return $this->pdo->prepare($sql)->execute($data);
 	}
+
+	// Getters
+	function getUserList()
+	{
+		// Returns all users as [id] => [username]
+		$sql = "SELECT username, id FROM users";
+		$obj = $this->pdo->prepare($sql);
+		$obj->execute();
+		$list = $obj->fetchAll(\PDO::FETCH_COLUMN);
+		array_unshift($list, NULL);
+		unset($list[0]);
+		return $list;
+	}
 }
