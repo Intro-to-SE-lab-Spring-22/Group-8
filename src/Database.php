7056b98f2289d5c6eee5e539683336516c0d0522
wolfgang-degroot <52136571+wolfgang-degroot@users.noreply.github.com>
2022-03-10 17:09:05 -0600

    "Fix" password.txt read error ICE

--- src/Database.php
+++ src/Database.php
@@ -24,7 +24,7 @@ class Database
 	protected function getCredentials()
 	{
 		// Get our database password.
-		return file_get_contents(__DIR__ . "/../password.txt");
+		return file_get_contents(__DIR__ . "/../password.txt") ?? "BobbyTables-8";
 	}
 
 	// Pipes
