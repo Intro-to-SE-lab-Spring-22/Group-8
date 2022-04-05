630ab5b59c2e975c0a0d8276a5429d05c7762a68
wolfgang-degroot <52136571+wolfgang-degroot@users.noreply.github.com>
2022-04-05 18:51:50 -0500

    Basic Auth functions

--- src/Auth.php
+++ src/Auth.php
@@ -1,14 +1,24 @@
 <?php
-#======================#
-# Spyke Authentication #
-# by Wolfgang de Groot #
-#======================#
 namespace Group8\Spyke;
 
-class Auth
+class Auth extends \Group8\Spyke\Database
 {
-    public function helloWorld()
-    {
-        print("You are not logged in...<br>Because nobody can.");
-    }
+	public function verifyPassword(int $id, string $password)
+	{
+		$stmt = $this->prepare("SELECT pass FROM users WHERE id = :id");
+		$stmt->bindParam(":id", $id);
+		$stmt->execute();
+		$result = $stmt->fetch();
+		if ($result) {
+			$hash = $result["pass"];
+			return password_verify($password, $hash);
+		}
+		return false;
+	}
+
+	public function whoAmI()
+	// Get the user's ID from the session.
+	{
+		return isset($_SESSION["user"]) ? $_SESSION["user"] : false;
+	}
 }
