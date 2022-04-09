2411929a46f090fd8e46c306cd630ddb399d39d2
wolfgang-degroot <52136571+wolfgang-degroot@users.noreply.github.com>
2022-04-08 21:28:49 -0500

    PostHandler now includes the username

--- src/PostHandler.php
+++ src/PostHandler.php
@@ -6,14 +6,35 @@ use \Group8\Spyke\Post as Post;
 class PostHandler extends \Group8\Spyke\Database\Post
 {
 	public int $user;
+	protected array $idMap;
 
 	protected function renderFeed(array $posts) {
 		$html = "";
 		foreach ($posts as $postData) {
 			$Post = new Post($postData);
+			$Post->username = $this->getUsernameFromID($Post->author);
 			$html .= $Post->render();
 		}
 		return $html;
+	}
+
+	private function getUsernameFromID(int $id) {
+		// Maps IDs to usernames.
+		// If we already looked one up,
+		// We skip it.
+		if ($this->idMap[$id]) {
+			return $this->idMap[$id];
+		} else {
+			$stmt = $this->prepare("SELECT username FROM users WHERE id = :id");
+			$stmt->bindParam(":id", $id);
+			$stmt->execute();
+			$result = $stmt->fetch();
+			if ($result) {
+				$this->idMap[$id] = $result[0];
+				return $result["username"];
+			}
+		}
+		return "Anonymous";
 	}
 
 	//? Feeds
