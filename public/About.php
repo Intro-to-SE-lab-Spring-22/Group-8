773d1b4c239747a928146260721f0f2a0cc62aa0
adb1042 <adb1042@msstate.edu>
2022-04-19 15:18:09 -0500

    About page addition
    Added About page
    added css for about page

---
+++ public/About.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<link rel="stylesheet" type="text/css"  href="assets/css/About.css">
+	
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>About Spyke</title>
+
+
+
+
+</head>
+
+
+<body>
+	<div class="Header">
+	<h1>ABOUT</h1>
+	<h3>What is spyke?</h3>
+	<p></p>
+	<p>Spyke is a new online social media platform dedicated to allowing our users to speak whats on their mind! Our users can say, well. Anything! as long as it does not include: </p>
+	<ol>
+	<li>Explicit material.</li>
+	<li>Malicious links or code.</li>
+	<li>Racial slurs or comment about one's race.</li>
+	</ol>
+	<h3>Who are we?</h3>
+	<p> Built by a group of college students, Spyke is open for everyone to enjoy!</p>
+	<h3>Who is Spyke meant for?</h3>
+	<p>Family, Friends, Pets, Your Pets Friends. Everyone! Spyke is still a work in progress, however we hope to continue to grow and become a staple part of keeping up with the world!</p>
+	<h4>We hope you enjoy Spyke as much as we do building it!</h4>
+	<a href="index.php">Spyke</a>
+	</div>
+	
+	
+
+
+
+
+
+</body>
+
+
+</html>
