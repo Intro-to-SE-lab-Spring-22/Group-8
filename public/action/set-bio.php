adf7f8ce70436535db0e9d2ab7e926b57fc59e7b
wolfgang-degroot <52136571+wolfgang-degroot@users.noreply.github.com>
2022-04-29 17:42:24 -0500

    Check if bio has fewer than 256 chars

--- public/action/set-bio.php
+++ public/action/set-bio.php
@@ -6,5 +6,12 @@ $UserDB = new Group8\Spyke\Database\User();
 $id = $_POST['id'] ?? 0;
 $bio = $_POST['bio'] ?? "";
 
-// Set the bio
-$UserDB->setBio($id, $bio);
+// Return false if the bio is
+// longer than 255 characters.
+if (strlen($bio) > 255) {
+	echo json_encode(false);
+	exit;
+} else {
+	// Set the bio.
+	echo json_encode($UserDB->setBio($id, $bio));
+}
