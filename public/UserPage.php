b5a698c3a6d63ceb77ce3e4c7e195dda13e56a82
adb1042 <adb1042@msstate.edu>
2022-04-29 19:29:14 -0500

    .
    .

--- public/UserPage.php
+++ public/UserPage.php
@@ -19,7 +19,7 @@ $Auth = new \Group8\Spyke\Auth;
 
 </head>
 <body>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>
 <script src="assets/js/UserPage.js"></script>
 
 <header>
